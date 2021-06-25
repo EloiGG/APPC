e5401e9f5cf12b01121d9f32169934243299d573
EloiGG <81160762+EloiGG@users.noreply.github.com>
2021-06-25 17:04:59 +0200

    réponse module

--- Source/SerialThread.cpp
+++ Source/SerialThread.cpp
@@ -10,13 +10,14 @@
 
 #include "SerialThread.h"
 
-SerialThread::SerialThread() : Thread("SerialThread"), progression(0.0f), exitAsked(false)
-{
-}
-
-SerialThread::SerialThread(const Sequence& s) : Thread("SerialThread"), sequence(s), progression(0)
+SerialThread::SerialThread() : Thread("SerialThread"), progression(0.0f), exitAsked(false), timeout_ms(3000)
 {
 	setPriority(9);
+}
+
+SerialThread::SerialThread(const Sequence& s) : SerialThread()
+{
+	sequence = s;
 }
 
 void SerialThread::setSequence(const Sequence& s)
@@ -32,9 +33,10 @@ float SerialThread::getProgression() const
 
 void SerialThread::run()
 {
-
 	auto delay = sequence.getDelay();
+
 	uart.open(1170, 8, UART::StopBit::oneStopBit, UART::Parity::noParity, 3);
+
 	wait(100);
 	for (int i = 0; i < sequence.getSize(); i++) {
 		const auto& step = sequence[i];
@@ -44,7 +46,9 @@ void SerialThread::run()
 		uart.addByte(step.character);	// caractère à afficher
 		uart.addByte(0x03);				// fin séquence
 		uart.send();
-		DBG(0x02 << step.order << step.adress << step.character << 0x03);
+
+		getModuleResponse(step.adress - 0x30, timeout_ms);
+
 		wait(delay);
 		progression = (float)i / (sequence.getSize() - 1);
 		if (threadShouldExit() || exitAsked)
@@ -57,5 +61,43 @@ void SerialThread::run()
 	{
 		Core::get().setInTransmission(false);
 		Core::get().updateVisualization();
+	}
+}
+
+ErrModule SerialThread::getModuleResponse(int digitNumber, int timeout_ms)
+{
+	ErrModule em;
+	s.reset();
+	unsigned char c = 0;
+	while (c != 0x02) {
+		s.wait_ms(30);
+		uart.read(&c); //...0x02
+		if (s.elapsed_time_ms() > timeout_ms) {
+			em.err_ok = false;
+			em.erreurs[em.err_reponse] = true;
+			return em;
+		}
+	}
+	uart.read(&c);		//0x52
+	uart.read(&c);		//<adresse>
+	if (c == digitNumber + 0x30) {
+		uart.read(&c);	//<code_err>
+		if (c == 0) {		// si pas d'erreur
+			em.err_ok = true;
+			return em;
+		}
+		else {				// si erreur
+			em.err_ok = false;
+			for (char b = 0b00000001, d = 'A', i = 0; d <= 'G'; b <<= 1, d++, i++)
+				if ((b & c) != 0) {
+					em.erreurs[i] = true;
+				}
+			return em;
+		}
+	}
+	else {
+		em.erreurs[em.err_illisible] = true;
+		em.err_ok = false;
+		return em;
 	}
 }
--- Source/SerialThread.h
+++ Source/SerialThread.h
@@ -14,7 +14,62 @@
 #include "Sequence.h"
 #include "UART.h"
 
+class Stopwatch
+{
+	using Clock = std::chrono::high_resolution_clock;
 
+private:
+	Clock::time_point start_point;
+
+public:
+	Stopwatch() :
+		start_point(Clock::now())
+	{}
+
+	void reset() { start_point = Clock::now(); }
+
+	double elapsed_time() const
+	{
+		std::atomic_thread_fence(std::memory_order_relaxed);
+		auto counted_time = std::chrono::duration_cast<std::chrono::seconds>(Clock::now() - start_point).count();
+		std::atomic_thread_fence(std::memory_order_relaxed);
+		return static_cast<double>(counted_time);
+	}
+	int elapsed_time_ms() const
+	{
+		std::atomic_thread_fence(std::memory_order_relaxed);
+		auto counted_time = std::chrono::duration_cast<std::chrono::milliseconds>(Clock::now() - start_point).count();
+		std::atomic_thread_fence(std::memory_order_relaxed);
+		return static_cast<int>(counted_time);
+	}
+	void wait(double seconds)
+	{
+		/*Clock::time_point start = Clock::now();
+		while (std::chrono::duration_cast<std::chrono::seconds>(Clock::now() - start).count() < seconds);*/
+		std::this_thread::sleep_for(std::chrono::duration<double>(seconds));
+	}
+	void wait_ms(double milliseconds)
+	{
+		wait(milliseconds / 1000.0);
+	}
+};
+
+struct ErrModule
+{
+	bool err_ok = 0;
+	bool erreurs[9] = { 0 };
+	enum {
+		err_A = 0,
+		err_B = 1,
+		err_C = 2,
+		err_D = 3,
+		err_E = 4,
+		err_F = 5,
+		err_G = 6,
+		err_reponse = 7,
+		err_illisible = 8
+	};
+};
 
 class SerialThread : public Thread
 {
@@ -24,10 +79,13 @@ public:
 	void setSequence(const Sequence& sequence);
 	float getProgression() const;
 	void askToExit() { exitAsked = true; }
+	ErrModule getModuleResponse(int digitNumber, int timeout_ms);
 private:
 	bool exitAsked;
 	float progression;
+	int timeout_ms;
 	virtual void run() override;
+	Stopwatch s;
 	UART uart;
 	Sequence sequence;
 };
